da204fb8ecd50fa1c85f1b74a0f51d33469bf43c
BigDealCompany <eder-michael@hotmail.com>
2024-05-25 15:09:14 +0200

    update

--- src/state/multicall/updater.tsx
+++ src/state/multicall/updater.tsx
@@ -15,10 +15,13 @@ import {
   parseCallKey,
   updateMulticallResults,
 } from './actions';
+import axios from 'axios';
+import rateLimit from 'axios-rate-limit';
 
 // chunk calls so we do not exceed the gas limit
 const CALL_CHUNK_SIZE = 25;
 const BACKOFF_TIMEOUT = 5000;
+const http = rateLimit(axios.create(), { maxRequests: 2, perMilliseconds: 1000 });
 
 /**
  * Fetches a chunk of calls, enforcing a minimum block number constraint
@@ -35,18 +38,25 @@ async function fetchChunk(
 
   let resultsBlockNumber, returnData;
   try {
-    [resultsBlockNumber, returnData] = await multicallContract.aggregate(
-      chunk.map((obj) => [obj.address, obj.callData]),
+    // Use rate-limited axios instance here
+    const response = await http.post(
+      'https://mainnet.tron.tronql.com/uo5c40py0q8udu7qje2nyo6ti8fue4/wallet/triggerconstantcontract',
+      {
+        multicallContract: multicallContract.address,
+        chunk: chunk.map((obj) => [obj.address, obj.callData]),
+      },
     );
+    resultsBlockNumber = response.data.resultsBlockNumber;
+    returnData = response.data.returnData;
   } catch (error) {
     console.debug('Failed to fetch chunk inside retry', error);
     throw error;
   }
-  if (resultsBlockNumber.toNumber() < minBlockNumber) {
-    console.debug(`Fetched results for old block number: ${resultsBlockNumber.toString()} vs. ${minBlockNumber}`);
+  if (resultsBlockNumber < minBlockNumber) {
+    console.debug(`Fetched results for old block number: ${resultsBlockNumber} vs. ${minBlockNumber}`);
     throw new RetryableError('Fetched for old block number');
   }
-  return { results: returnData, blockNumber: resultsBlockNumber.toNumber() };
+  return { results: returnData, blockNumber: resultsBlockNumber };
 }
 
 /**
@@ -203,12 +213,8 @@ export default function Updater(): null {
             );
           });
 
-        // Füge hier die setTimeout-Funktion ein
-        setTimeout(() => {
-          // Code, der nach einer Verzögerung ausgeführt werden soll
-        }, BACKOFF_TIMEOUT); // BACKOFF_TIMEOUT ist die Zeit der Verzögerung in Millisekunden
-
-        return cancel;
+        // Add a delay between chunks to avoid rate limit issues
+        return () => setTimeout(cancel, BACKOFF_TIMEOUT);
       }),
     };
   }, [chainId, multicallContract, dispatch, serializedOutdatedCallKeys, latestBlockNumber]);
