0f6b82df52353f5b84275a516af3d57d03821db2
BigDealCompany <eder-michael@hotmail.com>
2024-05-25 18:42:13 +0200

    update

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -24,7 +24,7 @@ interface tronWeb {
   };
   fullNode?: {
     chainType?: '';
-    host?: 'https://lightnode.intercroneswap.com';
+    host?: 'https://mainnet.tron.tronql.com/uo5c40py0q8udu7qje2nyo6ti8fue4/';
   };
 }
 
