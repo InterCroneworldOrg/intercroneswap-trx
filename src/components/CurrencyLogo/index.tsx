a87c0eb64f57562f348c4cfe9e5d92995459392e
InterCrone <86061927+InterCroneworld@users.noreply.github.com>
2026-08-27 15:45:16 +0200

    fix: show local TRX logo for native currency

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -2,14 +2,14 @@ import { Currency, ETHER, Token } from '@intercroneswap/v2-sdk';
 import { useMemo } from 'react';
 import styled from 'styled-components';
 
-import EthereumLogo from '../../assets/images/ethereum-logo.png';
+import TronLogo from '../../assets/images/trxlogo.png';
 import useHttpLocations from '../../hooks/useHttpLocations';
 import { WrappedTokenInfo } from '../../state/lists/hooks';
 import Logo from '../Logo';
 import { useAllLists } from '../../state/lists/hooks';
 import { getTokenLogoURL } from '../../utils/tokenLogo';
 
-const StyledEthereumLogo = styled.img<{ size: string }>`
+const StyledTronLogo = styled.img<{ size: string }>`
   width: ${({ size }) => size};
   height: ${({ size }) => size};
   box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
@@ -49,7 +49,7 @@ export default function CurrencyLogo({
   }, [currency, uriLocations]);
 
   if (currency === ETHER) {
-    return <StyledEthereumLogo src={EthereumLogo} size={size} style={style} />;
+    return <StyledTronLogo src={TronLogo} size={size} style={style} />;
   }
 
   return <StyledLogo size={size} srcs={srcs} alt={`${currency?.symbol ?? 'token'} logo`} style={style} />;
